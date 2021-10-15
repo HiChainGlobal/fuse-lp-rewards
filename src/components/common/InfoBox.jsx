0b9be4deb5477e327493e0b0598b151c02957e5a
ChenZenghui <524437472@qq.com>
2021-10-15 12:51:14 +0800

    Multi-language copy revision

--- src/components/common/InfoBox.jsx
+++ src/components/common/InfoBox.jsx
@@ -1,104 +1,138 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import classNames from 'classnames'
-import InfoIcon from '@/components/common/InfoIcon.jsx'
-import InfoIconHover from '@/components/common/InfoIconHover.jsx'
-import { useCountUp } from 'react-countup'
-import ReactModal from 'react-modal'
-import { useModal } from 'react-modal-hook'
-import { formatNumber } from '@/utils/format'
-import InfoIconModal from '@/assets/images/info-icon-modal.svg'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import classNames from "classnames";
+import InfoIcon from "@/components/common/InfoIcon.jsx";
+import InfoIconHover from "@/components/common/InfoIconHover.jsx";
+import { useCountUp } from "react-countup";
+import ReactModal from "react-modal";
+import { useModal } from "react-modal-hook";
+import { formatNumber } from "@/utils/format";
+import InfoIconModal from "@/assets/images/info-icon-modal.svg";
 import { useTranslation } from "react-i18next";
-export default ({ Icon, name, title, end, withSymbol = true, modalText, symbol, link, decimals, format = true }) => {
+export default ({
+  Icon,
+  name,
+  title,
+  end,
+  withSymbol = true,
+  modalText,
+  symbol,
+  link,
+  decimals,
+  format = true,
+}) => {
   let { t } = useTranslation();
-  const { accountAddress } = useSelector(state => state.network)
-  const [isHover, setHover] = useState(false)
-  const [modalStatus, setModalStatus] = useState(false)
+  const { accountAddress } = useSelector((state) => state.network);
+  const [isHover, setHover] = useState(false);
+  const [modalStatus, setModalStatus] = useState(false);
   const { countUp, start, update } = useCountUp({
     end,
     decimals,
-    ...(format && { formattingFn: formatNumber })
-  })
+    ...(format && { formattingFn: formatNumber }),
+  });
 
-  const [showModal] = useModal(() => (
-    
-    <ReactModal isOpen={modalStatus} overlayClassName='modal__overlay' className='modal__content'>
-      <div className='info-modal'>
-        <div className='image'><img src={InfoIconModal} /></div>
-        <div className='title'>
-         {t('liquidity_page_title_4_?_11')}“{title}”
-         {/* {t('liquidity_page_title_5_title_2')} */}
+  const [showModal] = useModal(
+    () => (
+      <ReactModal
+        isOpen={modalStatus}
+        overlayClassName="modal__overlay"
+        className="modal__content"
+      >
+        <div className="info-modal">
+          <div className="image">
+            <img src={InfoIconModal} />
+          </div>
+          <div className="title">
+            {t("liquidity_page_title_4_?_11")}“{title}”
+            {/* {t('liquidity_page_title_5_title_2')} */}
+          </div>
+          <div className="text">{modalText}</div>
+          <button
+            style={{ background: "#30DFC4" }}
+            className="close"
+            onClick={() => {
+              setModalStatus(false);
+            }}
+          >
+            {t("liquidity_page_button_4")}
+          </button>
         </div>
-        <div className='text'>
-          {modalText}
-        </div>
-        <button
-         style={{background:'#30DFC4'}}
-          className='close'
-          onClick={() => {
-            setModalStatus(false)
-          }}
-        >
-          {t('liquidity_page_button_4')}
-        </button>
-      </div>
-    </ReactModal>
-  ), [modalStatus])
+      </ReactModal>
+    ),
+    [modalStatus]
+  );
 
   useEffect(() => {
-    start()
-  }, [])
+    start();
+  }, []);
 
   useEffect(() => {
-    update(end)
-  }, [end])
+    update(end);
+  }, [end]);
 
   return (
-    <div className={classNames('info_box', { [`info_box--${name}`]: name && accountAddress }, { 'info_box--disabled': !accountAddress })}>
-      <div className='icons'>
+    <div
+      className={classNames(
+        "info_box",
+        { [`info_box--${name}`]: name && accountAddress },
+        { "info_box--disabled": !accountAddress }
+      )}
+    >
+      <div className="icons">
         <Icon />
         <div
           onClick={() => {
-            showModal()
-            setModalStatus(true)
+            showModal();
+            setModalStatus(true);
           }}
-          className='item'
+          className="item"
           onMouseEnter={() => {
-            setHover(true)
+            setHover(true);
           }}
           onMouseLeave={() => {
-            setHover(false)
+            setHover(false);
           }}
         >
-          {isHover && accountAddress ? <InfoIconHover fill='#7E8AB4' /> : <InfoIcon fill={accountAddress ? '#7E8AB4' : '#B2BCC4'} />}
+          {isHover && accountAddress ? (
+            <InfoIconHover fill="#7E8AB4" />
+          ) : (
+            <InfoIcon fill={accountAddress ? "#7E8AB4" : "#B2BCC4"} />
+          )}
         </div>
       </div>
       <div>
-        {
-          withSymbol
-            ? (
-              <div className={classNames('info_box__value', { 'info_box__value--disabled': !accountAddress })}>
-                {countUp}&nbsp;
-
-                {
-                  link ? (
-                    <a
-                      rel='noreferrer noopener'
-                      target='_blank'
-                      href={link}
-                    >
-                      {symbol}
-                    </a>
-                  ) : (
-                    <span>{symbol}</span>
-                  )
-                }
-              </div>
-            )
-            : <div  className={classNames('info_box__value', { 'info_box__value--disabled': !accountAddress })}>{countUp} %</div>
-        }
-        <div className={classNames('info_box__title', { 'info_box__title--disabled': !accountAddress })}>{title}</div>
+        {withSymbol ? (
+          <div
+            className={classNames("info_box__value", {
+              "info_box__value--disabled": !accountAddress,
+            })}
+          >
+            {countUp > 0 ? countUp : 0}&nbsp;
+            {link ? (
+              <a rel="noreferrer noopener" target="_blank" href={link}>
+                {symbol}
+              </a>
+            ) : (
+              <span>{symbol}</span>
+            )}
+          </div>
+        ) : (
+          <div
+            className={classNames("info_box__value", {
+              "info_box__value--disabled": !accountAddress,
+            })}
+          >
+            {countUp} %
+          </div>
+        )}
+        <div
+          className={classNames("info_box__title", {
+            "info_box__title--disabled": !accountAddress,
+          })}
+        >
+          {title}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
